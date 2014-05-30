0815af23580911ca0c8ba2ebdadf909e2ea7759f
Loris <loris@reexon.net>
2014-05-30 22:31:55 +0200

    Initial Commit

---
+++ GLibrary/GLibrary.h
@@ -0,0 +1,13 @@
+//
+//  GLibrary.h
+//  GLibrary
+//
+//  Created by Loris D'antonio on 30/05/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLibrary : NSObject
+
+@end
---
+++ GLibrary/GLibrary.m
@@ -0,0 +1,13 @@
+//
+//  GLibrary.m
+//  GLibrary
+//
+//  Created by Loris D'antonio on 30/05/14.
+//
+//
+
+#import "GLibrary.h"
+
+@implementation GLibrary
+
+@end

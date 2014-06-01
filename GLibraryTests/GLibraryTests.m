7f7c0631ce538a6c56809424b19e8fb9eab3cb89
Marco Velluto <velluto93@gmail.com>
2014-06-01 18:32:34 +0200

    Aggiunto Target GLibrary
    In questo target è inserita tutta la libreria, ricordarsi di
    aggiornarlo dopo ogni aggiunta di classi

---
+++ GLibraryTests/GLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  GLibraryTests.m
+//  GLibraryTests
+//
+//  Created by Marco Velluto on 01/06/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GLibraryTests : XCTestCase
+
+@end
+
+@implementation GLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
